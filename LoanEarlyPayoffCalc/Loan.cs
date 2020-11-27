71b390e7c1f27ec5ecbbc25e5a91a42c8ae536e6
Sarah Van Dyke <sarah.vandyke@waystar.com>
2020-11-27 12:46:59 -0500

    initial commit

---
+++ LoanEarlyPayoffCalc/Loan.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanEarlyPayoffCalc
+{
+    public class Loan
+    {
+        public Loan(string name, decimal principalRemaining, decimal interestRate,
+            decimal monthlyPayment)
+        {
+            Name = name;
+            PrincipalRemaining = principalRemaining;
+            InterestRate = interestRate;
+            MonthlyPayment = monthlyPayment;
+        }
+        public string Name { get; set; }
+        public decimal PrincipalRemaining { get; set; }
+        //public decimal InitialPrincipal { get; set; }
+        public decimal InterestRate { get; set; }
+        public decimal MonthlyPayment { get; set; }
+        private decimal InterestMultiplier => InterestRate / 12;
+        
+        public decimal CalculateBalanceAfterPayment()
+        {
+            var interest = CalculateInterest();
+            var newBalance = PrincipalRemaining - (MonthlyPayment - interest);
+            return newBalance;
+        }
+        public decimal CalculateInterest() { return InterestMultiplier * PrincipalRemaining; }
+
+    }
+}
--- LoanEarlyPayoffCalc/Program.cs
+++ LoanEarlyPayoffCalc/Program.cs
@@ -10,6 +10,24 @@ namespace LoanEarlyPayoffCalc
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Loan 1 Name?");
+            var name1 = Console.ReadLine();
+
+            Console.WriteLine("Principal Remaining?");
+            var prRemain1 = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Interest Rate?");
+            var interestRate1 = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Monthly Payment?");
+            var monthlyPayment1 = decimal.Parse(Console.ReadLine());
+
+            var loan1 = new Loan(name1, prRemain1, interestRate1, monthlyPayment1);
+        }
+
+        public decimal PrintAmoritizationSchedule(Loan loan)
+        {
+            throw new NotImplementedException();
         }
     }
 }
